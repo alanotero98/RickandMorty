8791677c95c9b09b530fefd97b208db5b9e58f30
Alan Otero <Oteroalan06@gmail.com>
2023-05-31 14:51:55 -0300

    Cambio de 31/05

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className='not'>
         <p className='numero'>404</p>
-        <p className='texto'>Page not found boludito</p>
+        <p className='texto'>Page not found</p>
       </div>
   )
 }
